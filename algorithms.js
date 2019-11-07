991f8b07ec1721d17fb837637c7ca68392c19be2
Serhii Romanovskyi <serhiir@ptua-wm0221.pt.playtech.corp>
2019-11-07 17:24:36 +0200

    add quick sort

--- algorithms.js
+++ algorithms.js
@@ -27,6 +27,49 @@ function binarySearch(array, target, start = 0, stop = array.length) {
         return binarySearch(array, target, start, midIndex - 1);
     }
 }
-console.log(
-    binarySearch([1, 2, 3, 4, 5, 7, 8, 9, 10, 44, 50, 77, 99, 100], 100)
-);
+// console.log(
+//     binarySearch([1, 2, 3, 4, 5, 7, 8, 9, 10, 44, 50, 77, 99, 100], 100)
+// );
+
+const arr = [5, 2, 1, 4, 3, 6];
+
+function sum(arr) {
+    return arr.length === 1 ? arr[0] : arr[0] + sum(arr.slice(1));
+}
+
+function findSmallest(arr) {
+    let smallest = arr[0];
+    arr.forEach(item => {
+        if (smallest > item) {
+            smallest = item;
+        }
+    });
+    return smallest;
+}
+
+// console.log(findSmallest(arr));
+
+function quickSort(arr) {
+    if (arr.length < 2) {
+        return arr;
+    } else {
+        const pivot = arr[Math.floor(Math.random() * arr.length)];
+        const greater = [];
+        const equal = [];
+        const less = [];
+        arr.forEach(item => {
+            if (item > pivot) {
+                greater.push(item);
+            }
+            if (item === pivot) {
+                equal.push(item);
+            }
+            if (item < pivot) {
+                less.push(item);
+            }
+        });
+        return [...quickSort(less), ...equal, ...quickSort(greater)];
+    }
+}
+
+console.log(quickSort([5, 3, 10, 200, 3, 2, 300, 401, 10, 500, 2]));
